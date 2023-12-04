e90f8c00de846f819eafde394993ed3f58f92a88
Rupert Foggo McKay <maxfmckay@gmail.com>
2023-12-04 09:02:01 +0100

    Day 04 Part 02 solved

--- 04/index.mjs
+++ 04/index.mjs
@@ -3,7 +3,7 @@
 /**
  * @param {string} inputLine
  */
-const scoreLine = (inputLine) => {
+const countMatchesOnCard = (inputLine) => {
   const [left, right] = inputLine.split("|");
   const winningSet = new Set(
     left
@@ -12,23 +12,22 @@ const scoreLine = (inputLine) => {
       .map((str) => parseInt(str))
       .filter(Boolean)
   );
-  const matches = right
+  return right
     .split(" ")
     .map((x) => parseInt(x))
     .filter(Boolean)
     .filter((x) => winningSet.has(x)).length;
+};
+
+/**
+ * @param {string} inputLine
+ */
+const scoreLine = (inputLine) => {
+  const matches = countMatchesOnCard(inputLine);
 
   if (matches === 0) return 0;
 
-  return (
-    2 **
-    (right
-      .split(" ")
-      .map((x) => parseInt(x))
-      .filter(Boolean)
-      .filter((x) => winningSet.has(x)).length -
-      1)
-  );
+  return 2 ** (matches - 1);
 };
 
 /**
@@ -36,3 +35,18 @@ const scoreLine = (inputLine) => {
  */
 export const solvePart1 = (inputLines) =>
   inputLines.map(scoreLine).reduce((a, b) => a + b);
+
+/**
+ * @param {string[]} inputLines
+ */
+export const solvePart2 = (inputLines) => {
+  const cardCounts = new Array(inputLines.length).fill(1);
+
+  for (let i = 0; i < inputLines.length; i++) {
+    const matches = countMatchesOnCard(inputLines[i]);
+    for (let j = 1; j < matches + 1; j++) {
+      cardCounts[i + j] += cardCounts[i];
+    }
+  }
+  return cardCounts.reduce((a, b) => a + b);
+};
--- 04/test.mjs
+++ 04/test.mjs
@@ -4,7 +4,7 @@ import { describe, test } from "node:test";
 
 import { getInputStrings } from "../utils.mjs";
 
-import { solvePart1 } from "./index.mjs";
+import { solvePart1, solvePart2 } from "./index.mjs";
 
 describe("Day 04", () => {
   describe("Part 01", () => {
@@ -13,6 +13,14 @@ describe("Day 04", () => {
     });
     test("real input", () => {
       strictEqual(solvePart1(getInputStrings("./04/input-real.txt")), 32001);
+    });
+  });
+  describe("Part 02", () => {
+    test("test input", () => {
+      strictEqual(solvePart2(getInputStrings("./04/input-test.txt")), 30);
+    });
+    test("real input", () => {
+      strictEqual(solvePart2(getInputStrings("./04/input-real.txt")), 5037841);
     });
   });
 });
