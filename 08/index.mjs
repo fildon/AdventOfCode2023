92e742f466439aea6e7bd83b922dfa6288b28668
Rupert McKay <rupert.mckay@lokalise.com>
2023-12-08 16:31:19 +0100

    Day 08 Part 02 work in progress

--- 08/index.mjs
+++ 08/index.mjs
@@ -1,6 +1,12 @@
 // @ts-check
 
-export const solvePart1 = (inputStrings) => {
+import { getInputStrings } from "../utils.mjs";
+
+/**
+ * @param {string[]} inputStrings
+ * @returns {{ directions: string, graph: Object.<string, { L: string, R: string }> }}}
+ */
+const parseInput = (inputStrings) => {
   const directions = inputStrings[0];
 
   const nodes = inputStrings.slice(2).map((nodeLine) => ({
@@ -14,6 +20,12 @@ export const solvePart1 = (inputStrings) => {
     return acc;
   }, {});
 
+  return { directions, graph };
+};
+
+export const solvePart1 = (inputStrings) => {
+  const { directions, graph } = parseInput(inputStrings);
+
   let location = "AAA";
   let steps = 0;
   let instructionPointer = 0;
@@ -26,3 +38,39 @@ export const solvePart1 = (inputStrings) => {
   }
   return steps;
 };
+
+const inputStrings = getInputStrings("./08/input-real.txt");
+
+const { directions, graph } = parseInput(inputStrings);
+
+const starts = Object.keys(graph).filter((key) => key.at(-1) === "A");
+
+starts.forEach((start) => {
+  let steps = 0;
+  let instructionPointer = 0;
+  let location = start;
+
+  /**
+   * @type {string[]}
+   */
+  const visitedStates = [];
+
+  while (!visitedStates.includes(`${location}:${instructionPointer}`)) {
+    visitedStates.push(`${location}:${instructionPointer}`);
+
+    const direction = directions[instructionPointer];
+    location = graph[location][direction];
+    steps++;
+    instructionPointer = (instructionPointer + 1) % directions.length;
+  }
+
+  console.log({
+    // start,
+    // loopStartNode: `${location}:${instructionPointer}`,
+    loopStartIndex: visitedStates.indexOf(`${location}:${instructionPointer}`),
+    loopLength:
+      visitedStates.length -
+      visitedStates.indexOf(`${location}:${instructionPointer}`),
+    // terminators: visitedStates.filter((x) => x[2] === "Z").length,
+  });
+})
